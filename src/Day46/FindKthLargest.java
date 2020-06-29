1f4b5266cd51e83054f13a2e1dec22b2788923ba
Viciivodka77 <Viciivodka77@outlook.com>
2020-06-29 18:34:52 +0800

    Day46 FindKthLargest

---
+++ src/Day46/FindKthLargest.java
@@ -0,0 +1,74 @@
+package Day46;
+
+import java.util.Arrays;
+
+public class FindKthLargest {
+
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+    //分治
+    public int findKthLargest1(int[] nums, int k) {
+        int len = nums.length;
+        int left = 0;
+        int right = len-1;
+        int target = len - k;
+        while (true) {
+            int index = partition(nums, left, right);
+            if (index == target) {
+                return nums[index];
+            } else if (index < target) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        int j = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < pivot){
+                j++;
+                swap(nums,i,j);
+            }
+        }
+        swap(nums,j,left);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    //快速排序理解分治
+    private static void quick_sort(int[] s, int l, int r)
+    {
+        if (l < r)
+        {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j)
+            {
+                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if(i < j)
+                    s[i++] = s[j];
+
+                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if(i < j)
+                    s[j--] = s[i];
+            }
+            s[i] = x;
+            quick_sort(s, l, i - 1); // 递归调用
+            quick_sort(s, i + 1, r);
+        }
+    }
+
+}
